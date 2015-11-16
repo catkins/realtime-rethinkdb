f0ee6cded2f9d926c0dc9ec2d32a61b8260e0797
Christopher Atkins <christopherlionelatkins@gmail.com>
2015-11-16 23:44:24 +1100

    Wire up basic express app to serve index page and static assets on prod environment

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
-setInterval(function() {
-  // do nothing yet
-  console.log(new Date())
-}, 10000)
+'use strict';
+
+// standard express app plumbing
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const path = require('path');
+
+app.use(morgan('dev'));
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  let template = 'index.html';
+
+  if (process.env.NODE_ENV === 'production') {
+    template = 'index.prod.html';
+  }
+
+  res.sendFile(path.join(__dirname, 'static', template));
+});
+
+const server = app.listen(3000, () => {
+  console.log(`Web server listening at http://localhost:${server.address().port}`);
+});
+
